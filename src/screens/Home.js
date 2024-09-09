403c48d35a6b81a8dc01e397b713daf0742c13fc
Kamva-pro <kamvelihlefatman@gmail.com>
2024-09-09 15:30:33 +0200

    imported styles

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,3 +1,4 @@
+import { StyleSheet, Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Entypo } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
