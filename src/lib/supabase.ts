56a87b1a4b62e28282d0c455879768c28448cf8a
Kamva-pro <kamvelihlefatman@gmail.com>
2024-09-14 11:50:56 +0200

    exported supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -53,7 +53,7 @@ class LargeSecureStore {
 const supabaseUrl = "https://fhsiqyrooyysetinpvwf.supabase.co/"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoc2lxeXJvb3l5c2V0aW5wdndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NzgyNjAsImV4cCI6MjA0MTQ1NDI2MH0.DFgLCEv23lZ32eF-Z0vYJ0Xo8nd0AmMHj08ShkXDd-M"
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: new LargeSecureStore(),
     autoRefreshToken: true,
@@ -61,3 +61,4 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     detectSessionInUrl: false,
   },
 });
+
--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://fhsiqyrooyysetinpvwf.supabase.co'
-
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoc2lxeXJvb3l5c2V0aW5wdndmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNTg3ODI2MCwiZXhwIjoyMDQxNDU0MjYwfQ.dNUSMmm_MJXtV2k-CWUmk1m_E9V8w3F1Guyg3LOTYVc"
-export const supabase = createClient(supabaseUrl, supabaseKey)
