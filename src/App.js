1e72be5651845fe68d04af995f373a19da1da652
Kamva-pro <kamvelihlefatman@gmail.com>
2024-10-11 01:30:23 +0200

    removed typescript

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import { View } from 'react-native'
 import { Session } from '@supabase/supabase-js'
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
+  // const [session, setSession] = useState<Session | null>(null)
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+  // useEffect(() => {
+  //   console.log('useEffect is triggered');  // Check if useEffect runs
+  
+  //   supabase.auth.getSession().then((response) => {
+  //     console.log('Response from getSession:', response);
+  //     const sessionData = response?.data?.session || null;
+  //     setSession(sessionData);
+  //   });
+  
+  //   supabase.auth.onAuthStateChange((_event, session) => {
+  //     console.log('Auth state changed:', session);
+  //     setSession(session);
+  //   });
+  // }, []);
+  
 
   return (
     <View>
-      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+      <Auth/>
+      {/* {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />} */}
     </View>
   )
 }
