06521c9390826eec45b1abe2d909edf368384d56
Kamva-pro <kamvelihlefatman@gmail.com>
2024-10-11 09:26:08 +0200

    removed supabase in app.js

--- src/App.js
+++ src/App.js
@@ -7,28 +7,10 @@ import {LoginScreen} from './screens/LoginScreen'
 import { Session } from '@supabase/supabase-js'
 
 export default function App() {
-  // const [session, setSession] = useState<Session | null>(null)
-
-  // useEffect(() => {
-  //   console.log('useEffect is triggered');  // Check if useEffect runs
-  
-  //   supabase.auth.getSession().then((response) => {
-  //     console.log('Response from getSession:', response);
-  //     const sessionData = response?.data?.session || null;
-  //     setSession(sessionData);
-  //   });
-  
-  //   supabase.auth.onAuthStateChange((_event, session) => {
-  //     console.log('Auth state changed:', session);
-  //     setSession(session);
-  //   });
-  // }, []);
-  
-
+ 
   return (
     <View>
       <LoginScreen/>
-      {/* {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />} */}
     </View>
   )
 }
