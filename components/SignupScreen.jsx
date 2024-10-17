5f2128b7053fd7a375f7acd023a8d9eebdeedacc
Kamva-pro <kamvelihlefatman@gmail.com>
2024-10-17 08:45:58 +0200

    created supabase client

--- components/SignupScreen.jsx
+++ components/SignupScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Alert } from 'react-native';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { supabase } from './supabase';  // Supabase config
+import { supabase } from '../supabase';  // Supabase config
 import { auth } from '../firebase';  // Firebase config
 
 export default function SignUpScreen() {
