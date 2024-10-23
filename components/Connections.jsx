2d09b98236292a09ba92f9f314021d9fbb71d96b
Kamva-pro <kamvelihlefatman@gmail.com>
2024-10-23 11:21:51 +0200

    connections

---
+++ components/Connections.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+function Connections() {
+    return (
+        <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
+            <Text>Connections Screen</Text>
+        </View>
+    )
+}
+
+export default Connections
