8f6da03e68b100e120c99795cf26685f94e39aeb
Kamva-pro <kamvelihlefatman@gmail.com>
2024-10-23 11:31:21 +0200

    nearby

--- components/NearbyConnections.jsx
+++ components/NearbyConnections.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Device from 'expo-device';
+import * as Location from 'expo-location';
 
+export default function Nearby() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-function Nearby() {
-    return (
-        <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
-            <Text>Nearby connections will appear here</Text>
-        </View>
-    )
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === 'android' && !Device.isDevice) {
+        setErrorMsg(
+          'Oops, this will not work on Snack in an Android Emulator. Try it on your device!'
+        );
+        return;
+      }
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
+  );
 }
 
-export default Nearby
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
