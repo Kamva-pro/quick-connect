3f7c0139b82fe5c14391244c91453483646db528
Kamva-pro <kamvelihlefatman@gmail.com>
2024-10-26 22:53:33 +0200

    events screen

---
+++ components/EventsScreen.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Text, View} from 'react-native';
+
+
+const Events = () => {
+    <Text>Nearby events you follow will appear here</Text>
+}
+
+export default Events;
