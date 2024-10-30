7d9d4e158807e791302bb0309dfcfacfea13ead8
Kamva-pro <kamvelihlefatman@gmail.com>
2024-10-30 09:24:39 +0200

    login screen logo

--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -49,7 +49,7 @@ const LoginScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
     
-      <Image width={250} height={250} src='../assets/logo.png'></Image>
+      <Image width={300} height={300} src='../assets/logo.png'></Image>
       <TextInput
         placeholder="Email"
         value={email}
