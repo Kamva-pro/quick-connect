64b0a9653b3afab1e4c22bda5965864d02747c1d
Kamva-pro <kamvelihlefatman@gmail.com>
2024-11-06 23:31:16 +0200

    styling for img

--- components/SignupScreen.jsx
+++ components/SignupScreen.jsx
@@ -128,27 +128,7 @@ const SignupScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 16,
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 8,
-  },
-  errorText: {
-    color: 'red',
-    marginBottom: 12,
-  },
-  loginText:{
-    color: "blue",
-    marginTop: 12,
-    textAlign: 'center'
-  },
+ 
   img: {
     width: "300",
     height: "300"
